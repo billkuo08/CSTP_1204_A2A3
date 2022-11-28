9b536380784d982b84cb17cf1d3f62259193a9ef
Bill Kuo <billkuo08@gmail.com>
2022-11-28 15:41:30 -0800

    Algorithms:bfs,dfs,shortestDistance

--- src/main/graph/Algorithms.java
+++ src/main/graph/Algorithms.java
@@ -1,32 +1,93 @@
 package main.graph;
-import main.staff.*;
 
+import main.staff.*;
 import java.util.*;
+import javax.naming.NameNotFoundException;
 
 
 public class Algorithms {
 
-	/**
-	 * *********************** Algorithms ****************************
-	 * 
-	 * Please see the README for the machine problem for a more detailed
-	 * specification of the behavior of each method that one should implement.
-	 */
-  
-	/**
-	 * This is provided as an example to indicate that this method and other
-	 * methods should be implemented here.
-	 * 
-	 * You should write the specs for this and all other methods.
-	 * 
-	 * @param graph
-	 * @param a
-	 * @param b
-	 * @return
-	 */
-	public static int shortestDistance(Graph graph, Vertex a, Vertex b) {
-	  // TODO: Implement this method and others
-	  return 0;
+	private static List<Vertex> dfsForVertex(Graph graph, Vertex start) {
+		Stack<Vertex> queue = new Stack<Vertex>();
+		List<Vertex> result = new ArrayList<Vertex>();
+		queue.push(start);
+		while (!queue.isEmpty()) {
+			Vertex up = queue.pop();
+			// Take the next element and add its downstream neighbors to the stack so that
+			// they are the next ones on which to carry the search
+			if (!result.contains(up)) {
+				result.add(up);
+				for (Vertex down : graph.getDownstreamNeighbors(up)) {
+					queue.push(down);
+				}
+			}
+		}
+		return result;
 	}
-  
-  }
+	
+	public static Set<List<Vertex>> depthFirstSearch(Graph graph) {
+		Set<List<Vertex>> result = new LinkedHashSet<List<Vertex>>();
+		for (Vertex vertex : graph.getVertices()) {
+			// Do a DFS starting at each vertex of the graph
+			result.add(Algorithms.dfsForVertex(graph, vertex));
+		}
+		return result;
+	}
+
+
+	private static Map<Vertex, Integer> bfsForVertex(Graph graph, Vertex start) {
+		Map<Vertex, Integer> result = new LinkedHashMap<Vertex, Integer>();
+		result.put(start, 0);
+		Queue<Vertex> queue = new LinkedList<Vertex>();
+		queue.add(start);
+		while (!queue.isEmpty()) {
+			// Take the first element in the queue
+			// Add all of its downstream neighbors to the queue to carry the BFS on them
+			// after the vertices in the current level are done
+			Vertex up = queue.remove();
+			// Add all of its downstream neighbors to the map with a depth one bigger than
+			// the depth of up
+			for (Vertex down : graph.getDownstreamNeighbors(up)) {
+				if (!result.containsKey(down)) {
+					queue.add(down);
+					result.put(down, result.get(up) + 1);
+				}
+			}
+		}
+		return result;
+	}
+
+
+	public static Set<List<Vertex>> breadthFirstSearch(Graph graph) {
+		// TODO: Implement this method
+		Set<List<Vertex>> result = new LinkedHashSet<List<Vertex>>();
+		for (Vertex vertex : graph.getVertices()) {
+			// Get a map with each vertex visited and the depth of it in the bfs starting
+			// from vertex
+			Map<Vertex, Integer> depths = Algorithms.bfsForVertex(graph, vertex);
+			List<Vertex> visitedVertices = new ArrayList<Vertex>();
+			// Add all the vertices visited into a list
+			for (Map.Entry<Vertex, Integer> entry : depths.entrySet()) {
+				visitedVertices.add(entry.getKey());
+			}
+			result.add(visitedVertices);
+		}
+		return result;
+	}	
+
+
+	public static int shortestDistance(Graph graph, Vertex a, Vertex b) throws NameNotFoundException {
+		// Do a BFS starting at vertex a and recording at what depth is each vertex
+		Map<Vertex, Integer> distances = Algorithms.bfsForVertex(graph, a);
+		if (!distances.containsKey(b)) {
+			// Condition is only true if b was not visited by the BFS of a
+			throw new NameNotFoundException ();
+		}
+
+		return distances.get(b);
+	}
+
+	
+	
+
+}
